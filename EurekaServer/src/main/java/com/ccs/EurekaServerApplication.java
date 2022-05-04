3894f14ff881fe03f8a0ad75d482d7a6a69cae73
Cheol-Soon-Choi <79562672+Cheol-Soon-Choi@users.noreply.github.com>
2022-05-04 12:08:23 +0900

    EurekaServer 구축

--- EurekaServer/src/main/java/com/ccs/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/ccs/EurekaServerApplication.java
@@ -2,14 +2,13 @@ package com.ccs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class MemberServiceApplication {
-
+@EnableEurekaServer
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MemberServiceApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
