83e7bbfa4a0b9e521b9c6e1ce71587d6b5806c5c
Cheol-Soon-Choi <79562672+Cheol-Soon-Choi@users.noreply.github.com>
2022-05-11 00:10:01 +0900

    zuulserver 추가

---
+++ ZuulServer/src/main/java/com/ccs/ZuulServerApplication.java
@@ -0,0 +1,16 @@
+package com.ccs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@RefreshScope
+public class ZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+}
