3a7a9991a9858834306598eefcbe0af53cea1e1f
Cheol-Soon-Choi <79562672+Cheol-Soon-Choi@users.noreply.github.com>
2022-05-12 00:28:30 +0900

    주울 prefilter 추가

--- ZuulServer/src/main/java/com/ccs/filters/PreFilter.java
+++ ZuulServer/src/main/java/com/ccs/filters/PreFilter.java
@@ -1,33 +1,60 @@
-//package com.ccs.filters;
-//
-//import com.netflix.zuul.ZuulFilter;
-//import com.netflix.zuul.exception.ZuulException;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class PreFilter extends ZuulFilter {
-//
-//    @Autowired
-//    FilterUtils filterUtils;
-//
-//    @Override
-//    public String filterType() {
-//        return null;
-//    }
-//
-//    @Override
-//    public int filterOrder() {
-//        return 0;
-//    }
-//
-//    @Override
-//    public boolean shouldFilter() {
-//        return false;
-//    }
-//
-//    @Override
-//    public Object run() throws ZuulException {
-//        return null;
-//    }
-//}
+package com.ccs.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PreFilter extends ZuulFilter {
+
+    private static final int FILTER_ORDER = 1;
+    private static final boolean SHOULD_FILTER = true;
+    private static final Logger logger = LoggerFactory.getLogger(PreFilter.class);
+
+    @Autowired
+    FilterUtils filterUtils;
+
+    @Override
+    public String filterType() {
+        return FilterUtils.PRE_FILTER_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FILTER_ORDER;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return SHOULD_FILTER;
+    }
+
+    @Override
+    public Object run() {
+        if (isCorrelationIdPresent()) {
+            logger.debug("!!!!! ccs-correlation-id found in tracking filter: {}. ", filterUtils.getCorrelationId());
+        } else {
+            filterUtils.setCorrelationId(generateCorrelationId());
+            logger.debug("!!!!! ccs-correlation-id generated in tracking filter: {}.", filterUtils.getCorrelationId());
+        }
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        logger.debug("!!!!! PreFilter Processing incoming request for {}.", ctx.getRequest().getRequestURI());
+        return null;
+    }
+
+    private boolean isCorrelationIdPresent() {
+        if (filterUtils.getCorrelationId() != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private String generateCorrelationId() {
+        return java.util.UUID.randomUUID().toString();
+    }
+}
