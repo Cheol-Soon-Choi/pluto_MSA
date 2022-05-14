d05c6c3f55b348c2befac2658e6808cd02ee56d2
Cheol-Soon-Choi <79562672+Cheol-Soon-Choi@users.noreply.github.com>
2022-05-14 16:03:10 +0900

    member 레디스 캐시 오타 수정

--- OrderServer/src/main/java/com/ccs/events/handlers/memberChangeHandler.java
+++ OrderServer/src/main/java/com/ccs/events/handlers/memberChangeHandler.java
@@ -2,7 +2,7 @@ package com.ccs.events.handlers;
 
 import com.ccs.events.CustomChannels;
 import com.ccs.events.models.MemberChangeModel;
-import com.ccs.repository.ItemRedisRepository;
+import com.ccs.repository.MemberRedisRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +13,7 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 @RequiredArgsConstructor
 public class memberChangeHandler {
 
-    private final ItemRedisRepository itemRedisRepository;
+    private final MemberRedisRepository memberRedisRepository;
     private static final Logger logger = LoggerFactory.getLogger(itemChangeHandler.class);
 
     @StreamListener("memberInput")
@@ -22,21 +22,21 @@ public class memberChangeHandler {
 
         switch (memberChangeModel.getAction()) {
             case "GET":
-                logger.debug("##### Received a GET event from the item server for Member id {}", memberChangeModel.getMemberId());
+                logger.debug("##### Received a GET event from the member server for Member id {}", memberChangeModel.getMemberId());
                 break;
             case "SAVE":
-                logger.debug("##### Received a SAVE event from the item server for Member id {}", memberChangeModel.getMemberId());
+                logger.debug("##### Received a SAVE event from the member server for Member id {}", memberChangeModel.getMemberId());
                 break;
             case "UPDATE":
-                logger.debug("##### Received a UPDATE event from the item server for Member id {}", memberChangeModel.getMemberId());
-                itemRedisRepository.deleteItem(memberChangeModel.getMemberId()); // 캐시 무효화
+                logger.debug("##### Received a UPDATE event from the member server for Member id {}", memberChangeModel.getMemberId());
+                memberRedisRepository.deleteMember(memberChangeModel.getMemberId()); // 캐시 무효화
                 break;
             case "DELETE":
-                logger.debug("##### Received a DELETE event from the item server for Member id {}", memberChangeModel.getMemberId());
-                itemRedisRepository.deleteItem(memberChangeModel.getMemberId()); // 캐시 무효화
+                logger.debug("##### Received a DELETE event from the member server for Member id {}", memberChangeModel.getMemberId());
+                memberRedisRepository.deleteMember(memberChangeModel.getMemberId()); // 캐시 무효화
                 break;
             default:
-                logger.error("##### Received an UNKNOWN event from the item server of type {}", memberChangeModel.getType());
+                logger.error("##### Received an UNKNOWN event from the member server of type {}", memberChangeModel.getType());
                 break;
 
         }
